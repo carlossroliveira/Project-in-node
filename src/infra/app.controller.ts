73a977dc2b21641ad6d9dfa3741a07fe40123c41
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2022-12-24 16:46:00 -0300

    Implementações de testes criações de classes

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'crypto';
 import { CreateNotificationBody } from './create-notification-body';
+
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
